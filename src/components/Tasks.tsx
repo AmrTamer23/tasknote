47891efffbc95422939fe3724e9eb6b3933ccfc3
Amr <amrtamer2324@gmail.com>
2023-09-17 22:50:10 +0300

    Added Animation to the Task page

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -15,21 +15,27 @@ const Tasks = () => {
 
     const handleTodayTasks = () => {
         setIsTodayTasksOpen(!isTodayTasksOpen);
+        const tasksSection = document.querySelector(".tasks.today");
+        tasksSection?.classList.toggle("open");
     }
+
+
 
     const [isUpTasksOpen, setIsUpTasksOpen] = useState(true);
 
     const handleUpTasks = () => {
         setIsUpTasksOpen(!isUpTasksOpen);
+        const tasksSection = document.querySelector(".tasks.upcoming");
+        tasksSection?.classList.toggle("open");
     }
 
     return (
         <div className="flex flex-col px-14 py-5 w-full ml-52 min-h-screen  bg-[#1E1E1E]">
             <h1>Good {timeOfDay}!</h1>
-            <div className="py-12 px-24">
+            <div className={'py-12 px-24'}>
                 <span className="flex justify-between pb-3">
                     <h2 className="text-3xl font-normal self-start">Today</h2>
-                    <div className="flex justify-center items-end"
+                    <div className="flex justify-center items-end cursor-pointer"
                         onClick={handleTodayTasks}
                     >
                         {
@@ -42,23 +48,19 @@ const Tasks = () => {
                     </div>
                 </span>
                 <hr />
-                {
-                    isTodayTasksOpen &&
-                    (<>
-                        <div className="flex flex-col pt-3">
-                            <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
-                            <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
-                            <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
-                            <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
-                            <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
-                        </div>
-                    </>)
-                }
+                <div className='flex flex-col pt-3 mt-3 tasks today'>
+                    <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
+                    <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
+                    <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
+                    <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
+                    <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
+                </div>
+
             </div>
             <div className="py-12 px-24">
                 <span className="flex justify-between pb-3">
-                    <h2 className="text-3xl font-normal self-start">UpComing</h2>
-                    <div className="flex justify-center items-end"
+                    <h2 className="text-3xl font-normal self-start">Upcoming</h2>
+                    <div className="flex justify-center items-end cursor-pointer"
                         onClick={handleUpTasks}
                     >
                         {
@@ -71,18 +73,15 @@ const Tasks = () => {
                     </div>
                 </span>
                 <hr />
-                {
-                    isUpTasksOpen &&
-                    (<>
-                        <div className="flex flex-col pt-3">
-                            <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
-                            <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
-                            <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
-                            <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
-                            <TaskItem taskName="Learn React And Apply" taskDue="Today" taskDesc="Wassap" />
-                        </div>
-                    </>)
-                }
+
+                <div className="flex flex-col pt-3 tasks upcoming">
+                    <TaskItem taskName="Learn React And Apply" taskDue="in 2 days" taskDesc="Wassap" />
+                    <TaskItem taskName="Learn React And Apply" taskDue="in 2 days" taskDesc="Wassap" />
+                    <TaskItem taskName="Learn React And Apply" taskDue="in 9 days" taskDesc="Wassap" />
+                    <TaskItem taskName="Learn React And Apply" taskDue="in 8 days" taskDesc="Wassap" />
+                    <TaskItem taskName="Learn React And Apply" taskDue="in 11 days" taskDesc="Wassap" />
+                </div>
+
             </div>
 
 
