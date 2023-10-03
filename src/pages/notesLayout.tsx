63154533315c7e88cad1d5c975f6cf3163c33e59
Amr Tamer <Amrtamer23mj@gmail.com>
2023-10-03 20:04:05 +0300

    added sound when delete note

--- src/pages/notesLayout.tsx
+++ src/pages/notesLayout.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Formik } from "formik";
 import FloatingButton from "../components/ui/floatingButton";
 import { FaPlus } from "react-icons/fa";
@@ -7,7 +7,7 @@ import Modal from "../components/ui/modal";
 import { useLocalStorageContext } from "../context/LocalStorageContext";
 import { NoteValues } from "../utils/interfaces";
 import NoteCreationForm from "../components/noteCreationForm";
-import { on } from "events";
+import removeSound from "../assets/sounds/remove-sound.mp3";
 import { TimeOfDay } from "../utils/helpers";
 
 function NotesLayout() {
@@ -19,72 +19,85 @@ function NotesLayout() {
 
   let { notes, addNote, lastNoteId, deleteNote } = useLocalStorageContext();
 
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  const playSound = () => {
+    audioRef.current!.volume = 0.2;
+    audioRef.current?.play();
+  };
+
   return (
-    <div className="flex flex-col">
-      <h1 className="font-semibold text-4xl">Good {TimeOfDay()}!</h1>
-      <div className="py-12 lg:px-24 grid lg:grid-cols-2 items-between justify-items-center align-items-center gap-5">
-        {notes &&
-          notes.map((note) => {
-            return (
-              <NoteItem
-                key={note.id}
-                note={
-                  {
-                    id: note.id,
-                    createdAt: note.createdAt,
-                    name: note.name,
-                    desc: note.desc,
-                    categoryId: note.categoryId,
-                    color: note.color,
-                  } as NoteValues
-                }
-                onDel={deleteNote}
-              />
-            );
-          })}
+    <>
+      <div className="flex flex-col">
+        <h1 className="font-semibold text-4xl">Good {TimeOfDay()}!</h1>
+        <div className="py-12 lg:px-24 grid lg:grid-cols-2 items-between justify-items-center align-items-center gap-5">
+          {notes &&
+            notes.map((note) => {
+              return (
+                <NoteItem
+                  key={note.id}
+                  note={
+                    {
+                      id: note.id,
+                      createdAt: note.createdAt,
+                      name: note.name,
+                      desc: note.desc,
+                      categoryId: note.categoryId,
+                      color: note.color,
+                    } as NoteValues
+                  }
+                  onDel={() => {
+                    deleteNote(note.id);
+                    playSound();
+                  }}
+                />
+              );
+            })}
 
-        <FloatingButton
-          Icon={FaPlus}
-          onClick={handleModal}
-          text="Add a Note"
-          children={
-            showModal && (
-              <Modal
-                onBackgroundClick={handleModal}
-                children={
-                  <>
-                    <h4 className="text-2xl self-center">Add a Note</h4>
-                    <Formik<NoteValues>
-                      initialValues={{
-                        id: lastNoteId() + 1,
-                        createdAt: new Date(),
-                        name: "",
-                        desc: "",
-                        categoryId: 0,
-                        color: "#333333",
-                      }}
-                      onSubmit={(values) => {
-                        addNote({
-                          id: values.id,
+          <FloatingButton
+            Icon={FaPlus}
+            onClick={handleModal}
+            text="Add a Note"
+            children={
+              showModal && (
+                <Modal
+                  onBackgroundClick={handleModal}
+                  children={
+                    <>
+                      <h4 className="text-2xl self-center">Add a Note</h4>
+                      <Formik<NoteValues>
+                        initialValues={{
+                          id: lastNoteId() + 1,
                           createdAt: new Date(),
-                          name: values.name,
-                          desc: values.desc,
-                          categoryId: values.categoryId,
-                          color: values.color,
-                        });
+                          name: "",
+                          desc: "",
+                          categoryId: 0,
+                          color: "#333333",
+                        }}
+                        onSubmit={(values) => {
+                          addNote({
+                            id: values.id,
+                            createdAt: new Date(),
+                            name: values.name,
+                            desc: values.desc,
+                            categoryId: values.categoryId,
+                            color: values.color,
+                          });
 
-                        if (values.name && values.desc) handleModal();
-                      }}
-                      component={NoteCreationForm}
-                    />
-                  </>
-                }
-              />
-            )
-          }
-        />
+                          if (values.name && values.desc) handleModal();
+                        }}
+                        component={NoteCreationForm}
+                      />
+                    </>
+                  }
+                />
+              )
+            }
+          />
+        </div>
       </div>
-    </div>
+      <audio src={removeSound} ref={audioRef}></audio>
+    </>
   );
 }
 
