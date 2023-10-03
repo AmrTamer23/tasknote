e0b25f15880ec6ed8f3b7120c2c9226cc010043c
Amr Tamer <Amrtamer23mj@gmail.com>
2023-10-03 19:08:51 +0300

    Fix the animation of the floating btn

--- src/components/ui/floatingButton.tsx
+++ src/components/ui/floatingButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { IconType } from "react-icons";
 
 interface FloatingButtonProps {
@@ -9,42 +9,16 @@ interface FloatingButtonProps {
 }
 
 function FloatingButton(props: FloatingButtonProps) {
-  const [isHovered, setIsHovered] = useState(false);
-
   const containerStyle =
-    "bg-[#28822C] text-white border-none px-2.5 py-5 text-lg cursor-pointer rounded-xl flex justify-center items-center gap-2 ease-linear transition-all duration-75";
+    "bg-[#28822C] text-white px-2.5 py-5 text-lg cursor-pointer rounded-xl flex justify-center items-center gap-2 opacity-70 hover:opacity-100 transition-all ease-in-out duration-300";
 
   return (
-    <div
-      className={"fixed bottom-5 right-5"}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <button
-        className={containerStyle}
-        onClick={props.onClick}
-        style={{
-          width: isHovered ? "150px" : "50px",
-          height: isHovered ? "70px" : "70px",
-          overflow: "hidden",
-        }}
-      >
+    <div className="fixed bottom-5 right-5">
+      <button className={containerStyle} onClick={props.onClick}>
         <props.Icon size={30} />
-        {isHovered && <p className="text-lg">{props.text}</p>}
+        <p className="text-lg">{props.text}</p>
       </button>
-      {isHovered && (
-        <div
-          className={
-            "absolute bottom-12 right-2  opacity-0 transition-opacity duration-100"
-          }
-        >
-          <div
-            className={"bg-gray-100 text-gray-800 rounded-lg py-1 px-3 text-sm"}
-          >
-            {props.text}
-          </div>
-        </div>
-      )}
+
       {props.children}
     </div>
   );
